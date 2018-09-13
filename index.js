e862360384900e0a4f6cd88ec0174cdf664b557b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-13 22:35:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
